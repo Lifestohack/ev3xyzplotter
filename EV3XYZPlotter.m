5ac484a9ba8ede628f8a7d46e5f497af6461ff5b
Diwas Bhattarai <diwas.bhattarai@web.de>
2019-12-13 17:55:42 +0100

    PID controller used . code that is not needed because of PID controller is deleted.

--- EV3XYZPlotter.m
+++ EV3XYZPlotter.m
@@ -1,27 +1,8 @@
-%ToDO:
-%1. Velocity profile is good but did not checked for end co-ordinates from
-%   encoder, so not getting same radius every time. 
-%2. Triangle test
-%3. PID controller overkill??
-%4. Position the shape as defined in the project
-%5. ??
-
-clc
 clear all
 if exist('ev3ShellHandle')
    clear ev3ShellHandle
 end
 
-% Axis as defined in the project
-% These sign for x and y are only valid for circle for this project
-%         |          x
-%   x+    |   x-     x
-%   y-    |   y-     x
-%---------|----------x
-%    x+   |   x-     x
-%    y+   |   y+     x
-%         |          x
-%yyyyyyyyyyyyyyyyyyyyy
 
 %Square Parameter
 l = 50; % in mm
@@ -32,48 +13,49 @@ centercircle = [25 25]; %in mm from 0,0
 
 %Traingle Parameter
 init = [0 0];
-mid = [50 25];
-final = [0 50];
+mid = [25 50];
+final = [50 0];
+
+% Axis as defined in the project
+%y         |          
+%y         |          
+%y         |          
+%y---------|----------
+%y         |          
+%y         |          
+%y         |          
+%yxxxxxxxxxxxxxxxxxxxxx
 
 %initializing variables
-global speed;
+
 global xm;
-global xdegree;
 global ym;
-global ydegree;
 global zm;
 global limitSwitch;
-global speedCircle;
+global speed;
+global smallGear;
+global bigGear;
 
-speed = 20; % default motor speed
-speedCircle = 30;
 xPort = 'A'; % ports in ev3
 yPort = 'B'; % ports in ev3
 zPort = 'C'; % ports in ev3
-xdegree = 2.04545; % degree per mm
-ydegree = 3; % degree per mm
+speed = 30; % default motor speed
+smallGear = 8;
+bigGear = 40;
 
-%making connection to EV3
-ev3ShellHandle = legoev3('USB');
+ev3ShellHandle = legoev3('USB'); %making connection to EV3
 
 %if wifi exists
 %ev3ShellHandle = legoev3('WiFi','ip address','hardware id'); 
 
 %initialize motor port
 xm = motor(ev3ShellHandle, xPort);
-stop(xm); %if motor was moving then stop
 xm.Speed = speed; %set speed
-
 ym = motor(ev3ShellHandle, yPort);
-stop(ym); %if motor was moving then stop
 ym.Speed = speed; %set speed
-
 zm = motor(ev3ShellHandle, zPort);
-stop(zm); %if motor was moving then stop
-zm.Speed = speed/10; %set speed
-
-%limit switch
-limitSwitch = touchSensor(ev3ShellHandle);
+zm.Speed = speed; %set speed
+limitSwitch = touchSensor(ev3ShellHandle); %limit switch
 
 %explicitly reset the initial motor degrees
 resetRotation(xm);
@@ -81,14 +63,14 @@ resetRotation(ym);
 resetRotation(zm);
 
 %Perform Performing Mechatronic systems task
-home(l) %home -> always run this function to home y. Make sure limit switch is connected
-engagePen(zm, 1) % pen down
+home(l) %home -> always run this function to home x. Make sure limit switch is connected
+%engagePen(zm, -1) % pen down
 drawQuadrilateral(l,l)
-drawCircle(centercircle, r);
 drawTriangle(init, mid, final)
-engagePen(zm, -1) % pen up
+drawCircle(centercircle, r);
+%engagePen(zm, 1) % pen up
 
-% terminate the connection
+%terminate the connection
 clear ev3ShellHandle
 
 %move pen up or down
@@ -98,7 +80,7 @@ function engagePen(zm, direction)
 %       zm : otor to move
 %       direction: 1 for up and -1 for down
     global speed;
-    start(zm , direction * (speed/2));
+    start(zm , direction * (30));
     changed = 1;
     i = 0;
     oldValue = readRotation(zm);
@@ -120,23 +102,24 @@ function home(l)
 %   Home Y axis
 %   Parameter
 %       l : max lenth before it stops if it didn't hit limit switch
+    global xm;
     global ym;
-    global ydegree;
     global limitSwitch;
     global speed;
-    maxdegree = l * ydegree;
-    ym.Speed = -speed;
-    start(ym);
+    xm.Speed = -speed;
+    start(xm);
     while ~readTouch(limitSwitch)
-        if readRotation(ym) > maxdegree
-            stop(ym);
-            disp('Maximum distance traveled for homing. If everything is fine then Please start again.')
-            break;
-        end
+%        disp(distancetraveled(xm))
+%         if distancetraveled(xm) > l
+%             stop(xm);
+%             disp('Maximum distance traveled for homing. If everything is fine then Please start again.')
+%             break;
+%         end
     end
-    stop(ym);
-    ym.Speed = speed;
+    stop(xm);
+    xm.Speed = speed;
     pause(1) % settling time for percussion measure
+    resetRotation(xm)
     resetRotation(ym)
 end
 
@@ -147,16 +130,11 @@ function drawQuadrilateral(l,b)
 %       l : lenth of one side 
 %       b : breadth of other side
     global xm;
-    global xdegree;
     global ym;
-    global ydegree;
-    global speed;
-    x = l * xdegree; %total degree to turn
-    y = b * ydegree; %total degree to turn
-    moveTo(xm, x, speed); % y is 0
-    moveTo(ym, y, speed); % x is 0
-    moveTo(xm, 0, speed); % y is 50
-    moveTo(ym, 0, speed); % x is 0
+    moveTo(xm, ym, 0, b)
+    moveTo(xm, ym, l, b)
+    moveTo(xm, ym, l, 0)
+    moveTo(xm, ym, 0, 0)
 end
 
 %Draw Circle
@@ -168,63 +146,18 @@ function drawCircle(centercircle, r)
 %            example : centercircle = [x,y];
 %       r : radius of circle
     global xm;
-    global xdegree;
     global ym;
-    global ydegree;
     global speed;
-    global speedCircle;
     x0 = centercircle(1);
     y0 = centercircle(2);
-    degree = 0:1:360;
-    x =  x0 + r*(cos((pi/180) * degree));
-    y =  y0 + r*(sin((pi/180) * degree));
-    if length(x) ~= length(y)
-        disp('WARNING!!! Length of x and y is not equal.')
+    degree = 0:10:360;
+    x = x0 + r*(cos((pi/180) * degree));
+    y = y0 + r*(sin((pi/180) * degree));
+    for i = 1:length(x)
+        moveTo(xm, ym, x(i), y(i))
     end
-    x = x .* xdegree
-    y = y .* ydegree
-    Vx = -speedCircle/r .* x;
-    Vy = speedCircle/r .* y;
-    disp(readRotation(xm))
-    disp(readRotation(ym))
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%Test for the circle%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     engagePen(zm, -1)
-%     moveTo(xm, 25*xdegree, speed)
-%     moveTo(ym, 25*ydegree, 1)
-%     engagePen(zm, 1)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    start(xm, 0);
-    start(ym, 0);
-    %Circle starts from [50,25] may be should start from [25, 0] array 271
-    %for x and y so this should be degree = 271:1:631;
-    for i = 1:length(Vx)
-        xm.Speed = Vx(i);
-        ym.Speed = Vy(i);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%Test for the circle%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%             if i == length(Vx)
-%                 i = 1;
-%             end            
-%             if  x(i+1) - x(i) < 0 &&  y(i+1) - y(i) > 0
-%                 %x- y+
-%                 
-%             elseif  x(i+1) - x(i) < 0 &&  y(i+1) - y(i) < 0
-%                 %x- y-
-%                 
-%             elseif  x(i+1) - x(i) > 0 &&  y(i+1) - y(i) < 0
-%                 %x+ y-
-%                 
-%             elseif  x(i+1) - x(i) > 0 &&  y(i+1) - y(i) > 0
-%                 %x+ y+
-%                 
-%             end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    end
-    stop(xm);
-    stop(ym);
-    xm.Speed  = speed;
-    ym.Speed  = speed;
+     xm.Speed  = speed;
+     ym.Speed  = speed;
 end
 
 %Draw Triangle
@@ -235,69 +168,78 @@ function drawTriangle(init, mid, final)
 %       mid : middle cordinate of triangle
 %       final : final cordinate of triangle
     global xm;
-    global xdegree;
     global ym;
-    global ydegree;
-    global speed;
-    %triangle first line
-    ym.Speed = (double(double(xm.Speed))/2); % set speed y = mx + b where b = 0 and m = 2
-    start(xm);
-    start(ym);
-    while readRotation(xm) < mid(1) * xdegree
-    end
-    stop(xm);
-    stop(ym);
-
-    %triangle second line
-    xm.Speed = -1 * xm.Speed;
-    start(xm);
-    start(ym);
-    while readRotation(xm) > final(1) * xdegree
-    end
-    stop(xm);
-    stop(ym);
-    xm.Speed = speed;
-    ym.Speed = speed;
+    moveTo(xm, ym, 25, 50)
+    moveTo(xm, ym, 50, 0)
+    %moveToPid(xm, ym, 0, 0)
 end
 
-%move one motor in a straight line
-function moveTo(motor, degree, speed)  
+%Move motor1 and motor2 simultaneously
+function moveTo(motor1, motor2, position1, position2)
 %   linear movement
 %   one motor at a time
 %   Parameters
-%       motor : motor to turn on
-%       degrees : turn motor specified degrees
-%       speed : move motor at specified speed
-    if readRotation(motor) < degree  %move forward
-        motor.Speed = speed; %set speed and direction
-        start(motor);
-        while readRotation(motor) < degree
+%       motor1 : first motor to turn on
+%       motor1 : second motor to turn on
+%       position1 : position for motor1 to run
+%       position2 : position for motor2 to run
+    mindistance = 2; %min of 5 distance to run otherwise exit the loop
+    stability = 0;   % if more than 9 stability in sequence then exit the loop
+    pid1 = PID(2, 3, 0, 0.02);
+    pid2 = PID(2, 3, 0, 0.02);
+    start(motor1,0);
+    start(motor2,0);
+    %time1 = clock;
+    while true
+        %time2 = clock;
+        %difftime = etime(time2,time1);
+        %time1 = clock;
+        %disp(difftime)
+        breakLoop = false;
+        measured1 = distancetraveled(motor1);
+        measured2 = distancetraveled(motor2);
+        out1 = pid1.cal(position1, measured1);
+        out2 = pid2.cal(position2, measured2);
+        if out1>=-mindistance && out1 <=  mindistance  && out2 >=  -mindistance && out2 <=  mindistance
+            stability = stability + 1;
         end
-    elseif readRotation(motor) > degree %move backward
-        motor.Speed = -speed; %set speed and direction
-        start(motor);
-        while readRotation(motor) > degree
+        motor1.Speed = out1;
+        motor2.Speed = out2;
+        if measured1 == position1 && measured2 == position2
+            stability = stability + 1;
+%         else 
+%             stability = 0;
+        end
+        if stability == 2
+            breakLoop = true;
+        end
+        if breakLoop
+            break;
         end
     end
-    stop(motor);
-    motor.Speed = speed;
+    stop(motor1)
+    stop(motor2)
 end
 
+%Get absolute distance travelled
 function distance = distancetraveled(motor)
-    global xdegree;
-    global ydegree;
-    global zdegree;
     global xm;
     global ym;
     global zm;
-    angle = readRotation(motor);
+    rxMotor = 20;
+    ryMotor = 23; %29
+    rzMotor = 1;
+    
+    global smallGear;
+    global bigGear;
+    theta = (smallGear/bigGear) * double(readRotation(motor));
     if isequal(xm,motor)
-        distance =  angle * xdegree
+        distance = 2 * pi * rxMotor * theta/360; 
     elseif isequal(ym,motor)
-        distance =  angle * ydegree
+        distance = 2 * pi * ryMotor * theta/360; 
     elseif isequal(zm,motor)
-        distance =  angle * zdegree
-    end  
+        distance = 2 * pi * rzMotor * theta/360; 
+    end
 end
 
 function stopMotors()
@@ -325,3 +267,4 @@ function objg = getGlobalObject()
     objg.zm = zm;
     objg.speed = speed;
 end
+
