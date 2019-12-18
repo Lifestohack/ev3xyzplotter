8f4bb5494dce36df9d012827ebdf25e9f844e2e7
Diwas Bhattarai <diwas.bhattarai@web.de>
2019-12-18 17:50:08 +0100

    segmentized the distance.
    z motor up and down
    kp ki values updated

--- EV3XYZPlotter.m
+++ EV3XYZPlotter.m
@@ -16,6 +16,7 @@ init = [0 0];
 mid = [25 50];
 final = [50 0];
 
+
 % Axis as defined in the project
 %y         |          
 %y         |          
@@ -27,7 +28,6 @@ final = [50 0];
 %yxxxxxxxxxxxxxxxxxxxxx
 
 %initializing variables
-
 global xm;
 global ym;
 global zm;
@@ -35,14 +35,15 @@ global limitSwitch;
 global speed;
 global smallGear;
 global bigGear;
+global minDis;
 
 xPort = 'A'; % ports in ev3
 yPort = 'B'; % ports in ev3
 zPort = 'C'; % ports in ev3
-speed = 30; % default motor speed
+speed = 50; % default motor speed
 smallGear = 8;
 bigGear = 40;
-
+minDis = 5;
 ev3ShellHandle = legoev3('USB'); %making connection to EV3
 
 %if wifi exists
@@ -50,10 +51,13 @@ ev3ShellHandle = legoev3('USB'); %making connection to EV3
 
 %initialize motor port
 xm = motor(ev3ShellHandle, xPort);
+stop(xm);
 xm.Speed = speed; %set speed
 ym = motor(ev3ShellHandle, yPort);
+stop(ym);
 ym.Speed = speed; %set speed
 zm = motor(ev3ShellHandle, zPort);
+stop(zm);
 zm.Speed = speed; %set speed
 limitSwitch = touchSensor(ev3ShellHandle); %limit switch
 
@@ -61,15 +65,20 @@ limitSwitch = touchSensor(ev3ShellHandle); %limit switch
 resetRotation(xm);
 resetRotation(ym);
 resetRotation(zm);
+%moveTo(xm,zm,0,10)
+%home(l)
+%moveTo(xm,ym,50,0)
 
 %Perform Performing Mechatronic systems task
 home(l) %home -> always run this function to home x. Make sure limit switch is connected
-%engagePen(zm, -1) % pen down
+moveTo(xm,zm,0,5)
 drawQuadrilateral(l,l)
+measured1 = distancetraveled(xm);
+measured2 = distancetraveled(ym);
 drawTriangle(init, mid, final)
 drawCircle(centercircle, r);
-%engagePen(zm, 1) % pen up
-
+engagePen(zm, -1) % pen up
+engagePen(zm, -1) % pen up
 %terminate the connection
 clear ev3ShellHandle
 
@@ -80,7 +89,7 @@ function engagePen(zm, direction)
 %       zm : otor to move
 %       direction: 1 for up and -1 for down
     global speed;
-    start(zm , direction * (30));
+    start(zm , direction * (50));
     changed = 1;
     i = 0;
     oldValue = readRotation(zm);
@@ -121,6 +130,11 @@ function home(l)
     pause(1) % settling time for percussion measure
     resetRotation(xm)
     resetRotation(ym)
+    %resetRotation(zm)
+    moveTo(xm, ym, 15, 0)
+    resetRotation(xm)
+    resetRotation(ym)
+    %resetRotation(zm)
 end
 
 %Draw Quadrilateral
@@ -131,10 +145,32 @@ function drawQuadrilateral(l,b)
 %       b : breadth of other side
     global xm;
     global ym;
-    moveTo(xm, ym, 0, b)
-    moveTo(xm, ym, l, b)
-    moveTo(xm, ym, l, 0)
+    moveTo(xm, ym, 0, 10)
+    moveTo(xm, ym, 0, 20)
+    moveTo(xm, ym, 0, 30)
+    moveTo(xm, ym, 0, 40)
+    moveTo(xm, ym, 0, 50)
+    
+    moveTo(xm, ym, 10, 50)
+    moveTo(xm, ym, 20, 50)
+    moveTo(xm, ym, 30, 50)
+    moveTo(xm, ym, 40, 50)
+    moveTo(xm, ym, 50, 50)
+    
+    moveTo(xm, ym, 50, 40)
+    moveTo(xm, ym, 50, 30)
+    moveTo(xm, ym, 50, 20)
+    moveTo(xm, ym, 50, 10)
+    moveTo(xm, ym, 50, 0)
+    
+    moveTo(xm, ym, 40, 0)
+    moveTo(xm, ym, 30, 0)
+    moveTo(xm, ym, 20, 0)
+    moveTo(xm, ym, 10, 0)
     moveTo(xm, ym, 0, 0)
+    
+    measured1 = distancetraveled(xm)
+    measured2 = distancetraveled(ym)
 end
 
 %Draw Circle
@@ -169,13 +205,72 @@ function drawTriangle(init, mid, final)
 %       final : final cordinate of triangle
     global xm;
     global ym;
-    moveTo(xm, ym, 25, 50)
+    
+    moveTo(xm, ym, 2.5, 5)
+    moveTo(xm, ym, 5, 10)
+    moveTo(xm, ym, 7.5, 15)
+    moveTo(xm, ym, 10, 20)
+    moveTo(xm, ym, 12.5, 25)
+    moveTo(xm, ym, 15, 30)
+    moveTo(xm, ym, 17.5, 35)
+    moveTo(xm, ym, 20, 40)
+    moveTo(xm, ym, 22.5, 45)
+    moveTo(xm, ym, 25, 50)             
+                    
+    moveTo(xm, ym, 27.5, 45)
+    moveTo(xm, ym, 30, 40)
+    moveTo(xm, ym, 32.5, 35)
+    moveTo(xm, ym, 35, 30)
+    moveTo(xm, ym, 37.5, 25)
+    moveTo(xm, ym, 40, 20)
+    moveTo(xm, ym, 42.5, 15)
+    moveTo(xm, ym, 45, 10)
+    moveTo(xm, ym, 47.5, 5)
     moveTo(xm, ym, 50, 0)
-    %moveToPid(xm, ym, 0, 0)
+    
+    measured1 = distancetraveled(xm)
+    measured2 = distancetraveled(ym)
+end
+
+function [xdot, ydot] = segmentize(x,y)
+    global xm;
+    global ym;
+    global minDis;
+    initalX = distancetraveled(xm);
+    initalY = distancetraveled(ym);
+    if initalX < x
+        xPrime = initalX:minDis:x;
+    else
+        xPrime = initalX:-minDis:x;
+    end
+    if initalY < y
+        yPrime = initalY:minDis:y;
+    else
+        yPrime = initalY:-minDis:y;
+    end
+    if length(yPrime) ~= length(xPrime)
+        if length(xPrime) == 1
+            xPrime = ones(1,length(yPrime),'uint32') * xPrime
+        end
+        if length(yPrime) == 1
+            yPrime = ones(1,length(xPrime),'uint32') * yPrime
+        end
+    end
+    xdot = xPrime;
+    ydot = yPrime;
+end
+
+function moveTo(motor1, motor2, position1, position2)
+%     [x,y] = segmentize(position1,position2);
+%     for i = 2:length(x)
+%         waitTill(motor1, motor2, x(i), y(i));
+%         dis = distancetraveled(motor2);
+%     end
+    waitTill(motor1, motor2, position1, position2);
 end
 
 %Move motor1 and motor2 simultaneously
-function moveTo(motor1, motor2, position1, position2)
+function waitTill(motor1, motor2, position1, position2)
 %   linear movement
 %   one motor at a time
 %   Parameters
@@ -185,21 +280,24 @@ function moveTo(motor1, motor2, position1, position2)
 %       position2 : position for motor2 to run
     mindistance = 2; %min of 5 distance to run otherwise exit the loop
     stability = 0;   % if more than 9 stability in sequence then exit the loop
-    pid1 = PID(2, 3, 0, 0.02);
-    pid2 = PID(2, 3, 0, 0.02);
+    pid1 = PID(1.71, 0.03622, 0, 0.02);
+    pid2 = PID(1.063, 0.020786, 0, 0.02);
+
     start(motor1,0);
     start(motor2,0);
-    %time1 = clock;
+%     time1 = clock;
+    breakLoop = false;
+    fileID = fopen('ymotor.txt','w');
     while true
-        %time2 = clock;
-        %difftime = etime(time2,time1);
-        %time1 = clock;
-        %disp(difftime)
-        breakLoop = false;
+%         time2 = clock;
+%         difftime = etime(time2,time1);
+%         time1 = clock;
+%         disp(difftime);
         measured1 = distancetraveled(motor1);
         measured2 = distancetraveled(motor2);
         out1 = pid1.cal(position1, measured1);
         out2 = pid2.cal(position2, measured2);
+        fprintf(fileID,'%6.2f,%6.2f,%6.2f\n',position2,measured2,out2);
         if out1>=-mindistance && out1 <=  mindistance  && out2 >=  -mindistance && out2 <=  mindistance
             stability = stability + 1;
         end
@@ -210,7 +308,7 @@ function moveTo(motor1, motor2, position1, position2)
 %         else 
 %             stability = 0;
         end
-        if stability == 2
+        if stability >= 30
             breakLoop = true;
         end
         if breakLoop
@@ -219,6 +317,7 @@ function moveTo(motor1, motor2, position1, position2)
     end
     stop(motor1)
     stop(motor2)
+    fclose(fileID);
 end
 
 %Get absolute distance travelled
@@ -226,9 +325,9 @@ function distance = distancetraveled(motor)
     global xm;
     global ym;
     global zm;
-    rxMotor = 20;
-    ryMotor = 23; %29
-    rzMotor = 1;
+    rxMotor = 19.5;
+    ryMotor = 27;
+    rzMotor = 50;
     
     global smallGear;
     global bigGear;
